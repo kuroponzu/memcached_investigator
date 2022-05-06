8f2efd29c94ffa1c96f5cf6942cec6ee1be70582
sho shimohata <pontatanpo1014@gmail.com>
2022-05-06 17:15:22 +0900

    Add display_response method

--- lib/memcached_investigator/client.rb
+++ lib/memcached_investigator/client.rb
@@ -10,29 +10,17 @@ module MemcachedInvestigator
 
     def stats
       sock.write("stats\r\n")
-      loop do
-        response = sock.readline(chomp: true)
-        p response
-        break if response.include?('END')
-      end
+      display_response
     end
 
     def get(key)
       sock.write("get #{key}\r\n")
-      loop do
-        response = sock.readline(chomp: true)
-        p response
-        break if response.include?('END')
-      end
+      display_response
     end
 
     def gets(key)
-      sock.write("get #{key}\r\n")
-      loop do
-        response = sock.readline(chomp: true)
-        p response
-        break if response.include?('END')
-      end
+      sock.write("gets #{key}\r\n")
+      display_response
     end
 
     def set(key:, value:, **option)
@@ -42,6 +30,56 @@ module MemcachedInvestigator
       sock.write("set #{key} #{flag} #{expire} #{size} \r\n#{value}\r\n")
       response = sock.readline(chomp: true)
       p response
+    end
+
+    def add(key:, size:, **option)
+      flag = option[:flag] || 0
+      expire = option[:expire] || (Time.now.to_i + 3600)
+      sock.write("set #{key} #{flag} #{expire} #{size}\r\n")
+      response = sock.readline(chomp: true)
+      p response
+    end
+
+    def replace(key:, size:, **option)
+      flag = option[:flag] || 0
+      expire = option[:expire] || (Time.now.to_i + 3600)
+      sock.write("set #{key} #{flag} #{expire} #{size}\r\n")
+      response = sock.readline(chomp: true)
+      p response
+    end
+
+    def append(key:, size:, **option)
+      flag = option[:flag] || 0
+      expire = option[:expire] || (Time.now.to_i + 3600)
+      sock.write("apend #{key} #{flag} #{expire} #{size}\r\n")
+      response = sock.readline(chomp: true)
+      p response
+    end
+
+    def prepend(key:, size:, **option)
+      flag = option[:flag] || 0
+      expire = option[:expire] || (Time.now.to_i + 3600)
+      sock.write("prepend #{key} #{flag} #{expire} #{size}\r\n")
+      response = sock.readline(chomp: true)
+      p response
+    end
+
+    def delete(key:)
+      sock.write("delete #{key}\r\n")
+      display_response
+    end
+
+    def flush_all(key:)
+      sock.write("flush_all\r\n")
+      display_response
+    end
+
+    private def display_response
+      loop do
+        response = sock.readline(chomp: true)
+        p response
+        break if response.include?('END')
+      end
     end
   end
 end
